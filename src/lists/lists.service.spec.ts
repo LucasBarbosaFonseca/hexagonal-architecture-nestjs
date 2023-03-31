8acc2ac69ab6a4109ac14f78c41f513f0cd7c610
LucasBarbosaFonseca <lufonsecabarbosa@hotmail.com>
2023-03-30 22:34:45 -0300

    hexagonal architecture with nest.js

---
+++ src/lists/lists.service.spec.ts
@@ -0,0 +1,33 @@
+import { ListGatewayInMemory } from './gateways/list-gateway-in-memory';
+import { ListsService } from './lists.service';
+
+describe('ListsService', () => {
+  let service: ListsService;
+  let listPersistenceGateway: ListGatewayInMemory;
+  let listIntegrationGateway: ListGatewayInMemory;
+
+  beforeEach(() => {
+    listPersistenceGateway = new ListGatewayInMemory();
+    listIntegrationGateway = new ListGatewayInMemory();
+    service = new ListsService(listPersistenceGateway, listIntegrationGateway);
+  });
+
+  it('should be able to create a list', async () => {
+    const list = await service.create({ name: 'my list' });
+    expect(listPersistenceGateway.items).toEqual([list]);
+    expect(listIntegrationGateway.items).toEqual([list]);
+  });
+  /*let service: ListsService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ListsService],
+    }).compile();
+
+    service = module.get<ListsService>(ListsService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });*/
+});
